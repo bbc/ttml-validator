65895cd92949b1685182f6381ab2bc6e24f7e353
Nigel Megitt <nigel.megitt@bbc.co.uk>
2025-01-30 17:56:50 +0000

    Add computeValue and validateValue methods
    Also fix regex for fontFamily

--- src/styleAttribs.py
+++ src/styleAttribs.py
@@ -1,6 +1,10 @@
 import re
-from dataclasses import dataclass
-from .xmlUtils import make_qname
+from collections.abc import Callable
+from dataclasses import dataclass, field
+from .xmlUtils import make_qname, get_unqualified_name
+# import logging
+import types
+from typing import TypeVar
 
 styling_ns_suffix = '#styling'
 ebutts_ns = 'urn:ebu:tt:style'
@@ -12,6 +16,15 @@ ebutt_distribution_color_type_regex = \
     r'(?P<b>[0-9a-fA-F]{2})' \
     r'(?P<a>[0-9a-fA-F]{2})?$'
 
+percent_regex = \
+    re.compile(r'^(?P<percent>[0-9]+(\.[0-9]+)?)%$')
+basis_regex = \
+    re.compile(r'^(?P<number>[0-9]+(\.[0-9]+)?)(?P<unit>[a-zA-Z%]*)$')
+
+specified_type = TypeVar('specified', bound=str)
+parent_type = TypeVar('parent', bound=str)
+params_type = TypeVar('params', bound=dict[str, str])
+
 
 @dataclass
 class StyleAttribute:
@@ -21,6 +34,126 @@ class StyleAttribute:
     appliesTo: list[str]
     syntaxRegex: re.Pattern
     defaultValue: str
+    computeValue: Callable[[specified_type, parent_type, params_type], str] = \
+        field(hash=False, compare=False)
+
+    def __post_init__(self):
+        # Bind the computeValue method to self so it can access
+        # object fields like syntaxRegex
+        self.computeValue = types.MethodType(self.computeValue, self)
+
+    def validateValue(self, value: str):
+        syntax_match = self.syntaxRegex.match(value)
+        return syntax_match is not None
+
+
+def _computeUninheritedAttribute(
+        self,
+        specified: str,
+        parent: str,
+        params: dict[str, str]
+) -> str:
+    return specified
+
+
+def _computeSimpleInheritedAttribute(
+        self,
+        specified: str,
+        parent: str,
+        params: dict[str, str]
+) -> str:
+    return parent if not specified else specified
+
+
+def _getPercentRelativeSize(
+        specified: str,
+        basis: str
+) -> str:
+    decoded_specified = percent_regex.match(specified)
+    if decoded_specified:
+        specified_float = float(decoded_specified.group('percent')) / 100
+    else:
+        raise ValueError(
+            'Specified value {} is not a valid percentage value'
+            .format(specified))
+
+    decoded_basis = basis_regex.match(basis)
+    if decoded_basis:
+        basis_float = float(decoded_basis.group('number'))
+        basis_unit = decoded_basis.group('unit')
+    else:
+        raise ValueError(
+            'Basis value {} is not a valid number'
+            .format(basis))
+
+    return '{:.6f}{}'.format(specified_float * basis_float, basis_unit)
+
+
+def _getCellHeight(params: dict[str, str]) -> str:
+    unq_key = 'cellResolution'
+    value = 100*1/15  # from default "32 15" value
+    unit = 'rh'
+    cellResolutionParams = {
+        key: value for key, value in params.items()
+        if get_unqualified_name(key) == unq_key}
+    if len(cellResolutionParams) == 1:
+        cellResolution = list(cellResolutionParams.values())[0]
+        columns = cellResolution.split()[1]
+        value = 100*1/int(columns)
+    elif len(cellResolutionParams) > 1:
+        raise ValueError('Too many {} parameters in {}'.format(
+            unq_key,
+            cellResolutionParams
+        ))
+
+    return '{:.6f}{}'.format(value, unit)
+
+
+def _computeFontSize(
+        self,  # Requires this to be used in the context of a StyleAttribute
+        specified: str,
+        parent: str,
+        params: dict[str, str]
+) -> str:
+    # if it's a %, compute relative to parent, or cell height if no parent
+    basis = parent if parent else _getCellHeight(params)
+
+    if specified:
+        specified_matches = self.syntaxRegex.match(specified)
+        if specified_matches:
+            specified_percent_val = specified_matches.group('percent')
+            rv = _getPercentRelativeSize(specified_percent_val, basis) \
+                if specified \
+                else basis
+        else:
+            # if it's not a %, it's not conformant EBU-TT-D
+            raise ValueError('{} is not a valid fontSize'.format(specified))
+    else:
+        rv = basis
+
+    # store in the params so lineHeight can be computed
+    params['fontSize'] = rv
+
+    return rv
+
+
+def _computeLineHeight(
+        self,
+        specified: str,
+        parent: str,
+        params: dict[str, str]
+) -> str:
+    rv = parent if parent else self.defaultValue
+
+    # if specified, compute relative to the computed font size
+    # (better have that in the params dict!)
+    if specified and specified != 'normal':
+        rv = _getPercentRelativeSize(specified, params['fontSize'])
+    elif specified and specified == 'normal':
+        rv = specified
+    # otherwise inherit the computed value from the parent
+
+    return rv
 
 
 styleAttribs = \
@@ -31,23 +164,27 @@ styleAttribs = \
             tag='direction',
             appliesTo=['p', 'span'],
             syntaxRegex=re.compile(r'^(ltr)|(rtl)$'),
-            defaultValue='ltr'
+            defaultValue='ltr',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
             nsIsRelative=True,
             tag='fontFamily',
             appliesTo=['span'],
-            syntaxRegex=re.compile(r'.*'),  # TODO
-            defaultValue='default'
+            syntaxRegex=re.compile(
+                r"""^(([-]?([_a-zA-Z]|[^\0-\237\\])([_a-zA-Z0-9\-]|[^\0-\237\\])*)([\s]+([-]?([_a-zA-Z]|[^\0-\237\\])([_a-zA-Z0-9\-]|[^\0-\237\\])*))*|(\"([^\"\\]|\\.)*\")|('([^'\\]|\\.)*'))([\s]*,[\s]*(([-]?([_a-zA-Z]|[^\0-\237\\])([_a-zA-Z0-9\-]|[^\0-\237\\])*)([\s]+([-]?([_a-zA-Z]|[^\0-\237\\])([_a-zA-Z0-9\-]|[^\0-\237\\])*))*|(\"([^\"\\]|\\.)*\")|('([^'\\]|\\.)*')))*$"""),
+            defaultValue='default',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
             nsIsRelative=True,
             tag='fontSize',
             appliesTo=['span'],
-            syntaxRegex=re.compile(r'^([\d]+(\.[\d]+)?%)$'),
-            defaultValue='100%'
+            syntaxRegex=re.compile(r'^(?P<percent>[\d]+(\.[\d]+)?%)$'),
+            defaultValue='100%',
+            computeValue=_computeFontSize
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -55,7 +192,8 @@ styleAttribs = \
             tag='lineHeight',
             appliesTo=['p'],
             syntaxRegex=re.compile(r'^(normal)|([\d]+(\.[\d]+)?%)$'),
-            defaultValue='100%'
+            defaultValue='normal',
+            computeValue=_computeLineHeight
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -64,7 +202,8 @@ styleAttribs = \
             appliesTo=['p'],
             syntaxRegex=re.compile(
                 r'^(left)|(center)|(right)|(start)|(end)|(justify)$'),
-            defaultValue='start'
+            defaultValue='start',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -72,7 +211,8 @@ styleAttribs = \
             tag='color',
             appliesTo=['span'],
             syntaxRegex=re.compile(ebutt_distribution_color_type_regex),
-            defaultValue='#ffffffff'
+            defaultValue='#ffffffff',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -80,7 +220,8 @@ styleAttribs = \
             tag='backgroundColor',
             appliesTo=['region', 'body', 'div', 'p', 'span'],
             syntaxRegex=re.compile(ebutt_distribution_color_type_regex),
-            defaultValue='#ffffffff'
+            defaultValue='#ffffffff',
+            computeValue=_computeUninheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -88,7 +229,8 @@ styleAttribs = \
             tag='fontStyle',
             appliesTo=['span'],
             syntaxRegex=re.compile(r'^(normal)|(italic)$'),
-            defaultValue='normal'
+            defaultValue='normal',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -96,7 +238,8 @@ styleAttribs = \
             tag='fontWeight',
             appliesTo=['span'],
             syntaxRegex=re.compile(r'^(normal)|(bold)$'),
-            defaultValue='normal'
+            defaultValue='normal',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -104,7 +247,8 @@ styleAttribs = \
             tag='textDecoration',
             appliesTo=['span'],
             syntaxRegex=re.compile(r'^(none)|(underline)$'),
-            defaultValue='none'
+            defaultValue='none',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -112,7 +256,8 @@ styleAttribs = \
             tag='unicodeBidi',
             appliesTo=['p', 'span'],
             syntaxRegex=re.compile(r'^(normal)|(embed)|(bidiOverride)$'),
-            defaultValue='normal'
+            defaultValue='normal',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -120,7 +265,8 @@ styleAttribs = \
             tag='wrapOption',
             appliesTo=['span'],
             syntaxRegex=re.compile(r'^(wrap)|(noWrap)$'),
-            defaultValue='wrap'
+            defaultValue='wrap',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=ebutts_ns,
@@ -128,7 +274,8 @@ styleAttribs = \
             tag='multiRowAlign',
             appliesTo=['p'],
             syntaxRegex=re.compile(r'^(auto)|(start)|(center)|(end)$'),
-            defaultValue='auto'
+            defaultValue='auto',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=ebutts_ns,
@@ -136,7 +283,8 @@ styleAttribs = \
             tag='linePadding',
             appliesTo=['p'],
             syntaxRegex=re.compile(r'^([\d]+(\.[\d]+)?c)$'),
-            defaultValue='0c'
+            defaultValue='0c',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=itts_ns,
@@ -144,7 +292,8 @@ styleAttribs = \
             tag='fillLineGap',
             appliesTo=['p'],
             syntaxRegex=re.compile(r'^(false)|(true)$'),
-            defaultValue='false'
+            defaultValue='false',
+            computeValue=_computeSimpleInheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -152,8 +301,11 @@ styleAttribs = \
             tag='origin',
             appliesTo=['region'],
             syntaxRegex=re.compile(
-                r'^([\d]+(\.[\d]+)?%)[\s]+([\d]+(\.[\d]+)?%)$'),
-            defaultValue='0% 0%'
+                r'^([0]*((100(\.[0]+)?)|[\d]{1,2}(\.[\d]+)?)%)'
+                r'[\s]+'
+                r'([0]*((100(\.[0]+)?)|[\d]{1,2}(\.[\d]+)?)%)$'),
+            defaultValue='0% 0%',
+            computeValue=_computeUninheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -161,8 +313,11 @@ styleAttribs = \
             tag='extent',
             appliesTo=['region'],
             syntaxRegex=re.compile(
-                r'^([\d]+(\.[\d]+)?%)[\s]+([\d]+(\.[\d]+)?%)$'),
-            defaultValue='100% 100%'
+                r'^([0]*((100(\.[0]+)?)|[\d]{1,2}(\.[\d]+)?)%)'
+                r'[\s]+'
+                r'([0]*((100(\.[0]+)?)|[\d]{1,2}(\.[\d]+)?)%)$'),
+            defaultValue='100% 100%',
+            computeValue=_computeUninheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -171,7 +326,8 @@ styleAttribs = \
             appliesTo=['region'],  # Only on region in EBU-TT-D
             syntaxRegex=re.compile(
                 r'^(before)|(center)|(after)$'),
-            defaultValue='before'
+            defaultValue='before',
+            computeValue=_computeUninheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -179,8 +335,9 @@ styleAttribs = \
             tag='padding',
             appliesTo=['region'],  # Only on region in EBU-TT-D
             syntaxRegex=re.compile(
-                r'^([\d]+(\.[\d]+)?%)([\s]+([\d]+(\.[\d]+)?%)){0-3}$'),
-            defaultValue='0%'
+                r'^([\d]+(\.[\d]+)?%)([\s]+([\d]+(\.[\d]+)?%)){0,3}$'),
+            defaultValue='0%',
+            computeValue=_computeUninheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -189,7 +346,8 @@ styleAttribs = \
             appliesTo=['region'],
             syntaxRegex=re.compile(
                 r'^(lrtb)|(rltb)|(tbrl)|(tblr)|(lr)|(rl)|(tb)$'),
-            defaultValue='lrtb'
+            defaultValue='lrtb',
+            computeValue=_computeUninheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -198,7 +356,8 @@ styleAttribs = \
             appliesTo=['region'],
             syntaxRegex=re.compile(
                 r'^(always)|(whenActive)$'),
-            defaultValue='always'
+            defaultValue='always',
+            computeValue=_computeUninheritedAttribute
         ),
         StyleAttribute(
             ns=styling_ns_suffix,
@@ -207,7 +366,8 @@ styleAttribs = \
             appliesTo=['region'],
             syntaxRegex=re.compile(
                 r'^(visible)|(hidden)$'),
-            defaultValue='hidden'
+            defaultValue='hidden',
+            computeValue=_computeUninheritedAttribute
         ),
         StyleAttribute(
             ns='',
@@ -218,7 +378,8 @@ styleAttribs = \
             # approximation. It may falsely permit some non-conformant values
             syntaxRegex=re.compile(
                 r'^([a-zA-Z_][\S]*([\t\f ]+([a-zA-Z_][\S]*))*)?$'),
-            defaultValue=''
+            defaultValue='',
+            computeValue=_computeUninheritedAttribute
         )
     ]
 
@@ -270,3 +431,12 @@ def attributeIsApplicableToElement(attr_key: str, el_tag: str) -> bool:
     return \
         attr_key not in _allAttributeKeys \
         or attr_key in _elementsToApplicableStyleAttributes.get(el_tag, set())
+
+
+def canonicaliseFontFamily(fontFamily: str) -> list[str]:
+    if fontFamily is None:
+        fontFamily = 'default'
+
+    return [
+        ff.strip() for ff in fontFamily.split(',')
+    ]
--- test/test_styleAttribs.py
+++ test/test_styleAttribs.py
@@ -2,7 +2,9 @@ import unittest
 import re
 from src.styleAttribs import getAllStyleAttributeKeys, \
     getStyleAttributeKeys, getStyleAttributeDict, \
-    StyleAttribute, attributeIsApplicableToElement
+    StyleAttribute, attributeIsApplicableToElement, \
+    getAllStyleAttributeDict, _getCellHeight, \
+    _getPercentRelativeSize, _computeUninheritedAttribute
 
 
 class testStyleAttribs(unittest.TestCase):
@@ -112,7 +114,8 @@ class testStyleAttribs(unittest.TestCase):
                  appliesTo=['style', 'region', 'body', 'div', 'p', 'span'],
                  syntaxRegex=re.compile(
                      r'^([a-zA-Z_][\S]*([\t\f ]+([a-zA-Z_][\S]*))*)?$'),
-                 defaultValue=''
+                 defaultValue='',
+                 computeValue=_computeUninheritedAttribute
             )
             }
         self.assertDictEqual(actual, expected)
@@ -144,3 +147,208 @@ class testStyleAttribs(unittest.TestCase):
                     attr_key=attr_key,
                     el_tag=el_tag
                 ))
+
+    def test_computeFontSize(self):
+        font_style_attr = \
+            getAllStyleAttributeDict(tt_ns='')['{#styling}fontSize']
+        specified = '150%'
+        parent = '100%'
+        params = {'{ttml#parameters}cellResolution': '32 16'}
+        computed = font_style_attr.computeValue(specified, parent, params)
+        self.assertEqual(computed[-1:], '%')
+        self.assertAlmostEqual(float(computed[:-1]), 150, 5)
+
+        parent = ''
+        computed = font_style_attr.computeValue(specified, parent, params)
+        self.assertEqual(computed[-2:], 'rh')
+        self.assertAlmostEqual(float(computed[:-2]), 9.375, 5)
+
+        params = {}
+        computed = font_style_attr.computeValue(specified, parent, params)
+        self.assertEqual(computed[-2:], 'rh')
+        self.assertAlmostEqual(float(computed[:-2]), 10, 5)
+
+        specified = '1c'
+        with self.assertRaises(ValueError) as ve:
+            computed = font_style_attr.computeValue(specified, parent, params)
+        self.assertEqual(
+            str(ve.exception),
+            '1c is not a valid fontSize'
+            )
+
+    def test_computeLineHeight(self):
+        line_height_attr = \
+            getAllStyleAttributeDict(tt_ns='')['{#styling}lineHeight']
+        specified = 'normal'
+        parent = 'normal'
+        params = {'fontSize': '7.5rh'}
+        computed = line_height_attr.computeValue(
+            specified=specified,
+            parent=parent,
+            params=params
+        )
+        self.assertEqual(computed, 'normal')
+
+        specified = '120%'
+        computed = line_height_attr.computeValue(
+            specified=specified,
+            parent=parent,
+            params=params
+        )
+        self.assertEqual(computed[-2:], 'rh')
+        self.assertAlmostEqual(float(computed[:-2]), 9, 5)
+
+        parent = '7.500000rh'
+        computed = line_height_attr.computeValue(
+            specified=specified,
+            parent=parent,
+            params=params
+        )
+        self.assertEqual(computed[-2:], 'rh')
+        self.assertAlmostEqual(float(computed[:-2]), 9, 5)
+
+        specified = ''
+        computed = line_height_attr.computeValue(
+            specified=specified,
+            parent=parent,
+            params=params
+        )
+        self.assertEqual(computed[-2:], 'rh')
+        self.assertAlmostEqual(float(computed[:-2]), 7.5, 5)
+
+    def test_getCellHeight(self):
+        params = {
+            '{ttml#parameters}cellResolution': '32 16'
+        }
+        result = _getCellHeight(params)
+        self.assertEqual(result, '6.250000rh')
+
+        # default of 1/15 should apply
+        params = {}
+        result = _getCellHeight(params)
+        self.assertEqual(result, '6.666667rh')
+
+        # too many params should raise exception
+        params = {
+            '{ttml#parameters}cellResolution': '32 16',
+            '{bogus}cellResolution': 'who cares'
+        }
+        with self.assertRaises(ValueError) as ve:
+            result = _getCellHeight(params)
+
+        self.assertEqual(
+            str(ve.exception),
+            "Too many cellResolution parameters in "
+            "{'{ttml#parameters}cellResolution': '32 16', "
+            "'{bogus}cellResolution': 'who cares'}"
+        )
+
+    def test_getPercentRelativeSize(self):
+        # Unitless
+        self.assertEqual(
+            _getPercentRelativeSize('100%', '100'), '100.000000'
+        )
+        # Percent of percent
+        self.assertEqual(
+            _getPercentRelativeSize('80%', '80%'), '64.000000%'
+        )
+        # Random units
+        self.assertEqual(
+            _getPercentRelativeSize('120%', '0.01freds'), '0.012000freds'
+        )
+
+        # Invalid percent
+        with self.assertRaises(ValueError) as ve:
+            _getPercentRelativeSize('10', 'nan')
+        self.assertEqual(
+            str(ve.exception), 
+            'Specified value 10 is not a valid percentage value')
+
+        # Invalid basis
+        with self.assertRaises(ValueError) as ve:
+            _getPercentRelativeSize('10%', 'nan')
+        self.assertEqual(
+            str(ve.exception), 
+            'Basis value nan is not a valid number')
+
+    def test_validateValue_ok(self):
+        good_values = [
+            ('{#styling}direction', 'rtl'),
+            ('{#styling}fontFamily', 'some string, another string, default'),
+            ('{#styling}fontSize', '12.45%'),
+            ('{#styling}lineHeight', 'normal'),
+            ('{#styling}lineHeight', '99.99%'),
+            ('{#styling}textAlign', 'start'),
+            ('{#styling}color', '#12abcd'),
+            ('{#styling}color', '#34abcdef'),
+            ('{#styling}backgroundColor', '#12abcd'),
+            ('{#styling}backgroundColor', '#34abcdef'),
+            ('{#styling}fontStyle', 'italic'),
+            ('{#styling}fontWeight', 'normal'),
+            ('{#styling}textDecoration', 'none'),
+            ('{#styling}unicodeBidi', 'bidiOverride'),
+            ('{#styling}wrapOption', 'noWrap'),
+            ('{urn:ebu:tt:style}multiRowAlign', 'center'),
+            ('{urn:ebu:tt:style}linePadding', '0.5c'),
+            ('{http://www.w3.org/ns/ttml/profile/imsc1#styling}fillLineGap', 'true'),
+            ('{#styling}origin', '10% 20.5%'),
+            ('{#styling}extent', '50.123% 30%'),
+            ('{#styling}padding', '1%'),
+            ('{#styling}padding', '1% 2.5%'),
+            ('{#styling}padding', '1.5% 2.5% 3%'),
+            ('{#styling}padding', '1.5% 2.5% 3% 5%'),
+            ('{#styling}writingMode', 'tblr'),
+            ('{#styling}showBackground', 'whenActive'),
+            ('{#styling}overflow', 'visible'),
+            ('style', 's1 style_other')
+        ]
+
+        allStyleDict = getAllStyleAttributeDict(tt_ns='')
+
+        for (style_attr_key, value) in good_values:
+            with self.subTest(
+                    style_attr_key=style_attr_key,
+                    value=value):
+                style_attr = allStyleDict.get(style_attr_key)
+                self.assertTrue(style_attr.validateValue(value=value))
+
+    def test_validateValue_bad(self):
+        good_values = [
+            ('{#styling}direction', 'forward'),
+            ('{#styling}fontFamily', '"badly quoted'),
+            ('{#styling}fontSize', '1'),
+            ('{#styling}fontSize', '3.5em'),
+            ('{#styling}lineHeight', '2c'),
+            ('{#styling}textAlign', 'weekend'),
+            ('{#styling}color', '#12abcg'),
+            ('{#styling}color', 'blueish'),
+            ('{#styling}backgroundColor', '#12abcg'),
+            ('{#styling}backgroundColor', 'blueish'),
+            ('{#styling}fontStyle', 'florid'),
+            ('{#styling}fontWeight', '3kg'),
+            ('{#styling}textDecoration', '3'),
+            ('{#styling}unicodeBidi', 'involuted'),
+            ('{#styling}wrapOption', 'golden'),
+            ('{urn:ebu:tt:style}multiRowAlign', 'arctic'),
+            ('{urn:ebu:tt:style}linePadding', '0.5%'),
+            ('{http://www.w3.org/ns/ttml/profile/imsc1#styling}fillLineGap', 'possible'),
+            ('{#styling}origin', '110% 20.5%'),
+            ('{#styling}extent', '-50.123% 30%'),
+            ('{#styling}padding', '1% '),
+            ('{#styling}padding', '1% 2.5px'),
+            ('{#styling}padding', '1.5% 2.5c 3px'),
+            ('{#styling}padding', 'quilt 1.5% 2.5% 3% 5%'),
+            ('{#styling}writingMode', 'random'),
+            ('{#styling}showBackground', 'curious'),
+            ('{#styling}overflow', 'niagara'),
+            ('style', 's1 1style_other')
+        ]
+
+        allStyleDict = getAllStyleAttributeDict(tt_ns='')
+
+        for (style_attr_key, value) in good_values:
+            with self.subTest(
+                    style_attr_key=style_attr_key,
+                    value=value):
+                style_attr = allStyleDict.get(style_attr_key)
+                self.assertFalse(style_attr.validateValue(value=value))
