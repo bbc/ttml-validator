e77261ae497963382841c49dce0415bb98b41989
Nigel Megitt <nigel.megitt@bbc.co.uk>
2025-12-19 16:00:59 +0000

    Fix indentation error

--- src/registries/jsonLoader.py
+++ src/registries/jsonLoader.py
@@ -12,20 +12,20 @@ def load_registry(file: str) -> List[Any]:
     with open(registry_path, mode='rt') as registry_file:
         registry_data = json.load(registry_file)
 
-    keys = [
-        column["rowKey"]
-        for column in registry_data["columns"]
-        if column.get("isKey", False)]
+        keys = [
+            column["rowKey"]
+            for column in registry_data["columns"]
+            if column.get("isKey", False)]
 
-    if len(keys) == 1:
-        registry_entries = [
-            value[keys[0]]
-            for value in registry_data["values"]
-        ]
-    else:
-        registry_entries = [
-            (value[key] for key in keys)
-            for value in registry_data["values"]
-        ]
+        if len(keys) == 1:
+            registry_entries = [
+                value[keys[0]]
+                for value in registry_data["values"]
+            ]
+        else:
+            registry_entries = [
+                (value[key] for key in keys)
+                for value in registry_data["values"]
+            ]
 
     return registry_entries
